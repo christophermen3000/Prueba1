64c18a0bd791705da77b18d7e35bd00366cb1bc4
christopher ilizarbe <christopher.ilizarbe@qdsgroup.com.pe>
2022-08-24 10:01:09 -0500

    commit oauth

---
+++ apigateway/src/main/java/pe/com/nttdata/apigateway/dto/RequestDto.java
@@ -0,0 +1,16 @@
+package pe.com.nttdata.apigateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class RequestDto {
+
+    private String uri;
+    private String method;
+}
